76e39dbc42e5bef4eaac2e71752f9cb5ad1fb690
Daniel Huber <dannyyhub@gmail.com>
2022-11-06 10:36:10 +0100

    Dodaj pliki projektów.

---
+++ Calculator.cs
@@ -0,0 +1,8 @@
+﻿namespace lab1
+{
+    public class Calculator
+    {
+        public uint Sum(uint a, uint b) => a+b;
+        public int Sub(int a, int b) => a-b;
+    }
+}
---
+++ HandlerCSV.cs
@@ -0,0 +1,44 @@
+﻿using CsvHelper;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Text;
+using System.Text.Json;
+
+namespace lab1
+{
+
+    class CSVData
+    {
+            public string A { get; set; }
+
+            public string B { get; set; }
+            public string Result { get; set; }
+    }
+
+
+    class HandlerCSV
+    {
+
+        string path = Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName + "data.csv";
+
+        public List<CSVData> data = new List<CSVData>();
+
+        public void ReadCSV()
+        {
+            using (var streemReader = new StreamReader(path))
+            {
+                using (var csv = new CsvReader(streemReader, CultureInfo.InvariantCulture))
+                {
+                    while (csv.Read())
+                    {
+                        var records = csv.GetRecord<CSVData>();
+                        data.Add(records);
+                        Console.WriteLine(records);
+                    }
+                }
+            }
+        }
+    }
+}
